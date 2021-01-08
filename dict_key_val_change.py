b910bd5583f237841cbe7a7fbb60b386846be006
Hande Yıldırım <38842051+handeyildirim@users.noreply.github.com>
2021-01-08 15:28:15 +0100

    Update dict_key_val_change.py

--- dict_key_val_change.py
+++ dict_key_val_change.py
@@ -1,5 +1,27 @@
-event_val = 0
 
+# example 1
+daq_entries = {
+    'V_SBC_1msState':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879135900, 'event': 1},
+    'V_DEM_SystemUpCounter': {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879243776, 'event': 1},
+    'V_TM_Counter':          {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 1},
+    'V_TM_Counter_1':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 1, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 2},
+    'V_TM_Counter_6':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 6, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 2},
+    'V_CSM_SYS_EcuControl':  {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 1, 'ecu_addr_ext': 0, 'ecu_addr': 1610653796, 'event': 2}
+}
+
+"""
+daq_entries = {
+    'V_SBC_1msState':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879135900, 'event': 2},
+    'V_DEM_SystemUpCounter': {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879243776, 'event': 2},
+    'V_TM_Counter':          {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 4, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 1},
+    'V_TM_Counter_1':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 1, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 2},
+    'V_TM_Counter_6':        {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 6, 'ecu_addr_ext': 0, 'ecu_addr': 1879167772, 'event': 2},
+    'V_CSM_SYS_EcuControl':  {'daq_list': -1, 'odt': -1, 'odt_entry': -1, 'bitoff': 255, 'size': 1, 'ecu_addr_ext': 0, 'ecu_addr': 1610653796, 'event': 1},
+
+}
+"""
+event_val = 0
+n = 0
 key_list = {}
 event_list = {}
 daq_list = {}
@@ -9,26 +31,20 @@ event_lists = []
 daq_list_entries = []
 odt_entries = []
 odt_entry_entries = []
-
 for key in daq_entries.items():
     main_keys = key[0]
     value = key[1]
     key_list[main_keys] = daq_entries[main_keys]
-
     event_list[main_keys] = key_list[key[0]]['event']
     current_event = event_list[main_keys]
     event_lists.append(current_event)
     event_list_length = len(event_list)
-
     daq_list[main_keys] = key_list[key[0]]['daq_list']
     current_daq_val = daq_list[main_keys]
-
     odt_list[main_keys] = key_list[key[0]]['odt']
     current_odt_val = odt_list[main_keys]
-
     odt_entry_list[main_keys] = key_list[key[0]]['odt_entry']
     current_odt_entry_val = odt_entry_list[main_keys]
-
     if event_val == current_event:
         if 'daq_list' in value:
             daq_entries[main_keys]['daq_list'] = daq_list_entries[-1]
@@ -69,14 +85,20 @@ for key in daq_entries.items():
             else:   # if odt_entry list is not empty
                 count_all_events = event_lists.count(current_event)
                 if count_all_events == 1:   # if it is first time to use current event
-                    # for i in range(event_list_length):
-                    index = max(index for index, item in enumerate(event_lists) if item == current_event)
-                    daq_entries[main_keys]['odt_entry'] = odt_entry_entries[-1] + index + 1
+                    if event_lists[n] <= event_lists[n+1]:
+                        daq_entries[main_keys]['odt_entry'] = odt_entry_entries[-1] + 1
+                    else:
+                        index = max(index for index, item in enumerate(event_lists) if item == current_event)
+                        daq_entries[main_keys]['odt_entry'] = odt_entry_entries[-1] + count_all_events + index
+                        odt_entry_entries.append(daq_entries[main_keys]['odt_entry'])
                     odt_entry_entries.append(daq_entries[main_keys]['odt_entry'])
                 else:   # if it is not first time to use current event
                     new_event_lists = event_lists[:-1]  # new list without last item of the current event list
                     final_index = max(index for index, item in enumerate(new_event_lists) if item == current_event)
                     daq_entries[main_keys]['odt_entry'] = odt_entry_entries[final_index] + 1
                     odt_entry_entries.append(daq_entries[main_keys]['odt_entry'])
+        n += 1
     event_val = current_event
     print('son: ' + str(daq_entries[key[0]]))
+
+

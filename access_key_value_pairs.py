11d9d804a7b43ecca23d537d4100a5230ee73bae
Hande Yıldırım <38842051+handeyildirim@users.noreply.github.com>
2021-01-19 13:29:14 +0100

    getting key-value pairs and values of a dictionary

--- access_key_value_pairs.py
+++ access_key_value_pairs.py
@@ -6,16 +6,33 @@ def key_value_accessing():
     "year": 1964
   }
   
-  # First, get the dict_values class to see what are values of the dict and to understand what values() method will return
+  # First, get the dict_values class which includes keys of the dictionary to understand what values() method will return
   # dict_value class will be as: dict_values(['Ford', 'Mustang', 1964])
   x = car.values()
   print(x)
   # you can see the type of x --> "class"
   print(type(x))
+  
+  # Then, get the dict_keys class to see what are values of the dict and to understand what values() method will return
+  # dict_value class will be as: dict_keys(['brand', 'model', 'year'])
   y = car.keys()
   print(y)
-  print(list(car.items())[0])
-  print((list(car.items())[0])[1])
+  # you can see the type of x --> "class"
+  print(type(y))
+  
+  # Now we can find each key-value pairs as elements of a tuple
+  
+  # For the first key-value pair:
+  print(list(car.items())[0])  # this print returns ('brand', 'Ford')
+  print((list(car.items())[0])[1]) # this print returns 'Ford'
+  
+  # For the second key-value pair:
+  print(list(car.items())[1])  # this print returns ('model', 'Mustang')
+  print((list(car.items())[1])[1]) # this print returns 'Mustang'
+  
+  # For the last key-value pair:
+  print(list(car.items())[2])  # this print returns ('year', '1964')
+  print((list(car.items())[2])[1]) # this print returns '1964'
 
 if __name__ == '__main__':
   call_func = key_value_accessing()
